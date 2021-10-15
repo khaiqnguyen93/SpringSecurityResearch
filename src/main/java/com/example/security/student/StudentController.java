3fd689dcfcbfb148a54bb94d093a3ef4f25bf53a
Khai Nguyen <khaiqnguyen93@gmail.com>
2021-10-15 21:50:12 +0700

    Add small API for later test

---
+++ src/main/java/com/example/security/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.security.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Jeff Bezos"),
+            new Student(2, "Bill Gates"),
+            new Student(3, "Elon Musk")
+    );
+    @GetMapping("/{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student " + studentId + "doesn't exist"));
+    }
+}
