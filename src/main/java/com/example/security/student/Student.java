e4c265312bc0eb9e603c8c13f81fd4ec2a6e18da
Khai Nguyen <khaiqnguyen93@gmail.com>
2021-10-15 21:54:11 +0700

    Add lombok

--- src/main/java/com/example/security/student/Student.java
+++ src/main/java/com/example/security/student/Student.java
@@ -1,19 +1,14 @@
 package com.example.security.student;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
-    private final Integer studentId;
-    private final String studentName;
+    private Integer studentId;
+    private String studentName;
 
-    public Student(Integer studentId, String studentName) {
-        this.studentId = studentId;
-        this.studentName = studentName;
-    }
-
-    public Integer getStudentId() {
-        return studentId;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
 }
